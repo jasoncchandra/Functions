83651462e0940f82dcc90014a500122030d30b42
jasoncchandra <jason.c.chandra@gmail.com>
2023-08-22 16:18:59 -0500

    committing

--- 2. Parsing HTML Stuff/Opentable.py
+++ 2. Parsing HTML Stuff/Opentable.py
@@ -3,42 +3,31 @@
 #pip install beautifulsoup4
 #In this version of the code, requests is used to fetch the HTML content of the webpage, and then BeautifulSoup is used to parse the HTML and extract the data you need. This approach doesn't require the use of a web browser, unlike the selenium approach.
 
-import re
 import pandas as pd
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-def parse_html(html):
-    data, item = pd.DataFrame(), {}
-    soup = BeautifulSoup(html, 'lxml')
-    for i, resto in enumerate(soup.find_all('div', class_='rest-row-info')):
-        item['name'] = resto.find('span', class_='rest-row-name-text').text
+# Define the URL
+url = "https://www.opentable.com/chicago-restaurants"
 
-        booking = resto.find('div', class_='booking')
-        item['bookings'] = re.search('\d+', booking.text).group() if booking else 'NA'
-
-        rating = resto.find('div', class_='star-rating-score')
-        item['rating'] = float(rating['aria-label'].split()[0]) if rating else 'NA'
-
-        reviews = resto.find('span', class_='underline-hover')
-        item['reviews'] = int(re.search('\d+', reviews.text).group()) if reviews else 'NA'
-
-        item['price'] = int(resto.find('div', class_='rest-row-pricing').find('i').text.count('$'))
-        item['cuisine'] = resto.find('span', class_='rest-row-meta--cuisine rest-row-meta-text sfx1388addContent').text
-        item['location'] = resto.find('span', class_='rest-row-meta--location rest-row-meta-text sfx1388addContent').text
-        data[i] = pd.Series(item)
-    return data.T
-
-# Use requests to get the HTML content
-url = "https://www.opentable.com/new-york-restaurant-listings"
+# Send a GET request to the URL
 response = requests.get(url)
-if response.status_code == 200:
-    page_source = response.text
-    new_data = parse_html(page_source)
-    new_data.to_csv('results.csv', index=False)
-    print("Data downloaded and parsed successfully.")
-else:
-    print("Failed to retrieve the page content.")
 
-restaurants = pd.read_csv('results.csv')
-print(restaurants)
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find the relevant information on the page (you need to inspect the page source to identify the correct tags)
+restaurant_names = [name.get_text() for name in soup.find_all("span", class_="rest-row-name-text")]
+restaurant_cuisines = [cuisine.get_text() for cuisine in soup.find_all("span", class_="rest-row-meta--cuisine")]
+restaurant_ratings = [rating.get_text() for rating in soup.find_all("span", class_="rest-row-num-rating")]
+
+# Create a DataFrame from the collected data
+data = {
+    "Restaurant Name": restaurant_names,
+    "Cuisine": restaurant_cuisines,
+    "Rating": restaurant_ratings
+}
+df = pd.DataFrame(data)
+
+# Display the DataFrame
+print(df)
